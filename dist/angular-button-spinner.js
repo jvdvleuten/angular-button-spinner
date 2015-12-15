55b8f6e0843b675811e527494efdddd8584989f3
Johan van der Vleuten <jvdvleuten@gmail.com>
2015-12-15 23:51:21 +0100

    Updated directive + demo.html

--- dist/angular-button-spinner.js
+++ dist/angular-button-spinner.js
@@ -2,55 +2,6 @@
     angular.module('angular-button-spinner', []).directive('buttonSpinner', ["$compile", function ($compile) {
         "use strict";
         
-        var link = function (scope, element, attributes) {
-            
-            var spinningIconElement, buttonAppendElement, buttonPrependElement;
-            
-            var spinningIconTemplateDefault = "<span ng-show=\"spinning\">&nbsp;<i class=\"fa fa-spinner fa-spin\"></i><span/>";
-            spinningIconElement = $compile(spinningIconTemplateDefault)(scope);
-            element.append(spinningIconElement);
-
-            attributes.$observe('spinningIcon', function (value) {
-                var spinningIconTemplate = "<span ng-show=\"spinning\">&nbsp;<i class=\"" + value + "\"></i><span/>";
-                if (!!spinningIconElement) {
-                    spinningIconElement.remove();
-                }
-                spinningIconElement = $compile(spinningIconTemplate)(scope);
-                element.append(spinningIconElement);
-            });
-
-            attributes.$observe('buttonAppend', function (value) {
-                if (!!buttonAppendElement) {
-                    buttonAppendElement.remove();
-                }
-                var buttonAppendTemplate = "<span ng-hide=\"spinning\">&nbsp;<i class=\"" + value + "\"></i><span/>";
-                buttonAppendElement = $compile(buttonAppendTemplate)(scope);
-                element.append(buttonAppendElement);
-            });
-
-            attributes.$observe('buttonPrepend', function (value) {
-                if (!!buttonPrependElement) {
-                    buttonPrependElement.remove();
-                }
-                var buttonPrependTemplate = "<span ng-hide=\"spinning\"><i class=\"" + value + "\"></i>&nbsp;<span/>";
-                buttonPrependElement = $compile(buttonPrependTemplate)(scope);
-                element.prepend(buttonPrependElement);
-
-                var spinningIconPrependTemplate = "<span ng-show=\"spinning\"><i class=\"fa fa-spinner fa-spin\"></i>&nbsp;<span/>";
-
-                if (scope.spinningIcon) {
-                    spinningIconPrependTemplate = "<span ng-show=\"spinning\"><i class=\"" + scope.spinningIcon + "\"></i>&nbsp;<span/>";
-                }
-                
-                if (!!spinningIconElement) {
-                    spinningIconElement.remove();
-                }
-                
-                spinningIconElement = $compile(spinningIconPrependTemplate)(scope);
-                element.prepend(spinningIconElement);
-            });
-        }
-
         return {
             restrict: 'A',
             scope: {
@@ -59,7 +10,13 @@
                 buttonPrepend: '@?',
                 buttonAppend: '@?'
             },
-            link: link
+            transclude: true,
+            template: 
+            "<span ng-if=\"!!buttonPrepend\" ng-hide=\"spinning\"><i class=\"{{ buttonPrepend }}\"></i>&nbsp;</span>" +
+            "<span ng-if=\"!!buttonPrepend\" ng-show=\"spinning\"><i class=\"{{ !!spinningIcon ? spinningIcon : 'fa fa-spinner fa-spin' }}\"></i>&nbsp;</span>" +
+            "<ng-transclude></ng-transclude>" +
+            "<span ng-if=\"!!buttonAppend\" ng-hide=\"spinning\">&nbsp;<i class=\"{{ buttonAppend }}\"></i></span>" +
+            "<span ng-if=\"!buttonPrepend\" ng-show=\"spinning\">&nbsp;<i class=\"{{ !!spinningIcon ? spinningIcon : 'fa fa-spinner fa-spin' }}\"></i></span>"
         }
     }]);
 } ());
